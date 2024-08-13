7dfea4888d453a3c841da565b655bca74eb0c506
mohamedfathy1991 <mohamed.electronics1991@gmail.com>
2024-08-13 16:42:28 +0300

    aa

--- src/midleware/globalerr.js
+++ src/midleware/globalerr.js
@@ -5,7 +5,7 @@ const  globalerr = (err,req,res,next)=>{
       const statuscode = err.statusCode||500
       res.status(statuscode).json({
             errorr:err.message,
-            stack:err.stack ,
+           
             code:statuscode
       })
       
